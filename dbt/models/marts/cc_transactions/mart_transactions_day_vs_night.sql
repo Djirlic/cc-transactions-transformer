26de7778fb2d121d2c4f78331a79a767b37dfd75
Manuel Djirlic <freelance@djirlic.com>
2025-06-28 20:34:31 +0000

    ADD gold layer transformations, docs, and tests for segmentation by weekday and day and night separation

---
+++ dbt/models/marts/cc_transactions/mart_transactions_day_vs_night.sql
@@ -0,0 +1,20 @@
+SELECT
+    is_night_transaction,
+    COUNT(*) AS total_transactions,
+    SUM(CASE WHEN is_fraud THEN 1 ELSE 0 END) AS fraudulent_transactions,
+    SUM(amount) AS total_amount,
+    SUM(CASE WHEN is_fraud THEN amount ELSE 0 END) AS fraud_amount,
+    ROUND(
+        SUM(CASE WHEN is_fraud THEN 1 ELSE 0 END)::FLOAT / NULLIF(COUNT(*), 0),
+        4
+    ) AS fraud_rate,
+    COALESCE(
+        ROUND(
+            SUM(CASE WHEN is_fraud THEN amount ELSE 0 END)::FLOAT /
+            NULLIF(SUM(CASE WHEN is_fraud THEN 1 ELSE 0 END), 0),
+            4
+        ),
+        0
+    )  AS avg_fraud_amount_per_transaction
+FROM {{ ref('int_transactions_enriched') }}
+GROUP BY is_night_transaction
